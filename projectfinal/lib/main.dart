ecccdc89aaf7d64820bcb7087ece0053320ce0bf
unknown <r.aimer@esi-sba.dz>
2024-03-14 23:37:31 +0000

    I fixed the splash screen

--- projectfinal/lib/main.dart
+++ projectfinal/lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:projectfinal/firebase_options.dart';
+import 'package:projectfinal/pages/home_page.dart';
 import 'package:projectfinal/pages/home_screen.dart';
 import 'package:projectfinal/pages/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Initialize Flutter framework
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
--- projectfinal/lib/pages/splash_screen.dart
+++ projectfinal/lib/pages/splash_screen.dart
@@ -1,49 +1,63 @@
 import "dart:async";
-
-import "package:flutter/material.dart";
-import "package:flutter/widgets.dart";
-import "package:flutter_svg/svg.dart";
-import "package:projectfinal/Theme/Colors.dart";
-import 'package:flutter_svg/flutter_svg.dart';
-import "package:projectfinal/pages/login_screen.dart";
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:projectfinal/Theme/Colors.dart';
+import 'package:projectfinal/pages/Verify_emailPage.dart';
+import 'package:projectfinal/pages/home_page.dart';
+import 'package:projectfinal/pages/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-   void initState() {
+  @override
+ void initState() {
     super.initState();
-    Timer(
-      Duration(seconds: 5),
-      () => Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
-      ),
+    Future.delayed(const Duration(seconds: 3), () {
+        if (FirebaseAuth.instance.currentUser != null) {
+    if (FirebaseAuth.instance.currentUser!.emailVerified == false) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (BuildContext context) => VerifyEmail()),
+      );
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (BuildContext context) => HomePage()),
+      );
+    }
+  } else {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
     );
-   }
+  }
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.background,
-      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0,),
-      body:  Center(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-           
-           Image.asset("assets/Icons/logo 2.png", width: 130, height: 102.78,),
-           Image.asset("assets/Icons/Name.png", width: 145,height:44),
-           Image.asset("assets/Icons/Slogan.png", width: 152,height:19),
-           SizedBox(height: 100,),
-
-            
+            Image.asset("assets/Icons/logo 2.png", width: 130, height: 102.78),
+            Image.asset("assets/Icons/Name.png", width: 145, height: 44),
+            Image.asset("assets/Icons/Slogan.png", width: 152, height: 19),
+            SizedBox(height: 100),
           ],
         ),
       ),
     );
-
   }
 }
+
+
