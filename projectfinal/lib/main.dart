d7fc61cfa24e50693a9d1ad3a8eb467e2b348367
Ines AINI <i.aini@esi-sba.dz>
2024-04-12 14:20:38 +0200

    initial commit

--- projectfinal/lib/main.dart
+++ projectfinal/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:projectfinal/firebase_options.dart';
 import 'package:projectfinal/pages/home_screen.dart';
+import 'package:projectfinal/pages/homepage%20work/home_page.dart';
+import 'package:projectfinal/pages/notifications_page.dart';
 
 
 Future<void> main() async {
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: HomeScreen());
+    return   MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
