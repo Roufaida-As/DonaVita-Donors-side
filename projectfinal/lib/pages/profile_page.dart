796d247c2e8810bfe73e661583da932920d1b128
Mansouri-Fatima <fz.mansouri@esi-sba.dz>
2024-04-13 23:52:33 +0100

    add profile page

--- projectfinal/lib/pages/profile_page.dart
+++ projectfinal/lib/pages/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projectfinal/Theme/Colors.dart';
+import 'package:projectfinal/pages/edit_profile_screen.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,8 +10,124 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  // ignore: constant_identifier_names
+  static const double IMAGE_HORIZENTAL_PADDING = 24;
+  void _goToEditProfilePage() {
+    setState(() {
+      Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const EditProfileScreen()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          leading: const BackButton(
+            color: AppColors.icons,
+          ),
+          actions: [
+            TextButton(
+                onPressed: _goToEditProfilePage,
+                child: const Text('Edit profile',
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: AppColors.icons,
+                        fontWeight: FontWeight.bold))),
+          ],
+
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: IMAGE_HORIZENTAL_PADDING),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Center(
+                      child: ClipOval(
+                    child: Material(
+                      color: Colors.transparent,
+                      child: Image(
+                        image: AssetImage("assets/Icons/Rectangle 7.png"),
+                        width: 128,
+                        height: 128,
+                      ),
+                    ),
+                  )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Full Name",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: AppColors.icons,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const TextField(
+                    enabled: false,
+                    readOnly: true,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        disabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: AppColors.icons),
+                        ),
+                        label: Text(
+                          "Email",
+                          style:
+                              TextStyle(fontSize: 13, color: AppColors.icons),
+                        ),
+                        fillColor: AppColors.font),
+                  ),
+                  const TextField(
+                    enabled: false,
+                    readOnly: true,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                        disabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: AppColors.icons),
+                        ),
+                        label: Text(
+                          "Phone number",
+                          style:
+                              TextStyle(fontSize: 13, color: AppColors.icons),
+                        ),
+                        fillColor: AppColors.font),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Row(
+                        children: [
+                          Icon(
+                            Icons.logout_rounded,
+                            color: Colors.red,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Log out",
+                            style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red),
+                          )
+                        ],
+                      ))
+                ],
+              )),
+        ));
   }
 }
