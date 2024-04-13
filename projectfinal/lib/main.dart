25803ed8927f1df7d63c8ff7478487bf0a25ed4c
Mansouri-Fatima <fz.mansouri@esi-sba.dz>
2024-04-14 00:39:07 +0100

    fix : change the start Screen from home_page to HomeScreen()

--- projectfinal/lib/main.dart
+++ projectfinal/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   MaterialApp(
-        debugShowCheckedModeBanner: false, home: HomePage());
+    return const  MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
