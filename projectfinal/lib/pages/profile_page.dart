69f6bbd0931c78333e2790017a8c3650adec27a9
Mansouri-Fatima <fz.mansouri@esi-sba.dz>
2024-05-05 01:49:13 +0100

    add signout singUp logic

--- projectfinal/lib/pages/profile_page.dart
+++ projectfinal/lib/pages/profile_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:projectfinal/Theme/Colors.dart';
 import 'package:projectfinal/pages/edit_profile_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:projectfinal/pages/login_screen.dart';
+
+FirebaseAuth auth = FirebaseAuth.instance;
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,13 +14,60 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  // this function should be remove right after the login page is ready
+  Future<void> signIn() async {
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+        email: "fz.mansouri@esi-sba.dz",
+        password: "updated-password",
+      );
+      print("SignIn success");
+
+      // Handle successful login
+    } catch (e) {
+      print("Failed to sign in: $e");
+    }
+  }
+
+// this funtion should be updated after the login logic is done
+  Map loadUserInfoFromSharedPref() {
+    return {
+      "email": "fz.mansouri@esi-sba.dz",
+      "password": "updated-password",
+      "name": "Fatima mansouri"
+    };
+  }
+  
   // ignore: constant_identifier_names
   static const double IMAGE_HORIZENTAL_PADDING = 24;
-  void _goToEditProfilePage() {
+  Future<void> _goToEditProfilePage() async {
     setState(() {
       Navigator.of(context).push(
           MaterialPageRoute(builder: (context) => const EditProfileScreen()));
     });
+  }
+
+  Future<void> _signOut() async {
+    await FirebaseAuth.instance.signOut();
+    setState(() {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const LoginPage()));
+    });
+  }
+
+// this function to create users since the signUp is not ready yet , it should be removed after the login is done
+  Future<void> signUp() async {
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: "fz.mansouri@esi-sba.dz",
+        password: "fatima12345",
+      );
+      // You can do additional operations here, like saving user data to Firestore
+
+      print("SignUp success");
+    } catch (e) {
+      print("Failed to sign up: $e");
+    }
   }
 
   @override
@@ -32,7 +83,6 @@ class _ProfilePageState extends State<ProfilePage> {
                         color: AppColors.icons,
                         fontWeight: FontWeight.bold))),
           ],
-
         ),
         body: SizedBox(
           width: double.infinity,
@@ -81,7 +131,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         label: Text(
                           "Email",
                           style:
-                              TextStyle(fontSize: 13, color: AppColors.icons),
+                              TextStyle(fontSize: 17, color: AppColors.icons),
                         ),
                         fillColor: AppColors.font),
                   ),
@@ -96,7 +146,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         label: Text(
                           "Phone number",
                           style:
-                              TextStyle(fontSize: 13, color: AppColors.icons),
+                              TextStyle(fontSize: 17, color: AppColors.icons),
                         ),
                         fillColor: AppColors.font),
                   ),
@@ -104,7 +154,7 @@ class _ProfilePageState extends State<ProfilePage> {
                     height: 32,
                   ),
                   TextButton(
-                      onPressed: () {},
+                      onPressed: () async => {await _signOut()},
                       child: const Row(
                         children: [
                           Icon(
